0cde4aeed6872f2c55aa898fd25592c14b54f13b
Alexander Suslov <sasha_suslov07@mail.ru>
2022-05-06 16:53:14 +0300

    reafactor: change api instance name

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,14 +2,14 @@
 import axios from 'axios';
 import { API_URL, TOKEN } from '../app/constants/authorization';
 
-const $api = axios.create({
+const api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
 });
 
-$api.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
   config.headers!.Authorization = `Bearer ${localStorage.getItem(TOKEN)}`;
   return config;
 });
 
-export default $api;
+export default api;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,16 +1,16 @@
 import { AxiosResponse } from 'axios';
-import $api from '../api';
+import api from '../api';
 import Endpoint from '../app/constants/endpoints';
 import { NewUser, User } from '../types/user';
 
 export default class AuthService {
   static async signin<SingInResponse>(user: User): Promise<AxiosResponse<SingInResponse>> {
-    return $api.post(Endpoint.SIGN_IN, user);
+    return api.post(Endpoint.SIGN_IN, user);
   }
 
   static async signup<SingUpResponse>(
     newUser: NewUser,
   ): Promise<AxiosResponse<SingUpResponse>> {
-    return $api.post(Endpoint.SIGN_UP, newUser);
+    return api.post(Endpoint.SIGN_UP, newUser);
   }
 }
