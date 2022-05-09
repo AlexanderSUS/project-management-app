314c7e48a3d094d0178232f7863ad8e565880027
PanchenkoAleksandr <aleksandr.panchenko.1989@gmail.com>
2022-05-10 00:32:59 +0300

    feat: add footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const Layout = () => (
   <>
     <Header />
     <Outlet />
+    <Footer />
   </>
 );
 export default Layout;
