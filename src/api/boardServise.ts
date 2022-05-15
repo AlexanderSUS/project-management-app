ab91403370b911367096bf71ba0c72837b323a0e
Alexander Suslov <sasha_suslov07@mail.ru>
2022-05-15 11:37:36 +0300

    refactor: fix board service

--- src/api/boardServise.ts
+++ src/api/boardServise.ts
@@ -1,23 +1,24 @@
 import { AxiosResponse } from 'axios';
 import api from '.';
+import Endpoint from '../constants/endpoints';
 import {
   BoardId, Boards, BoardType, NewBoard,
 } from '../types/boards';
 
 export default class BoardService {
   static fetchBoards(): Promise<AxiosResponse<Boards>> {
-    return api.get('/boards');
+    return api.get(Endpoint.BOARDS);
   }
 
   static createBoard(data: NewBoard): Promise<AxiosResponse<BoardType>> {
-    return api.post('/boards', data);
+    return api.post(Endpoint.BOARDS, data);
   }
 
   static deleteBoard(id: BoardId): Promise<AxiosResponse<Boards>> {
-    return api.delete(`/boards/${id}`);
+    return api.delete(`${Endpoint.BOARDS}/${id}`);
   }
 
   static editBoard(data: BoardType): Promise<AxiosResponse<BoardType>> {
-    return api.put(`/boards/${data.id}`, data.title);
+    return api.put(`${Endpoint.BOARDS}/${data.id}`, data.title);
   }
 }
