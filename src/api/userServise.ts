a2dad62eeb2804b601dea4967f7ef3cd40c476ed
Alexander Suslov <sasha_suslov07@mail.ru>
2022-05-20 16:54:07 +0300

    feat: update log in logic and fix edit profile request logic

--- src/api/userServise.ts
+++ src/api/userServise.ts
@@ -6,18 +6,18 @@ import { SignUpFormInput } from '../types/authTypes';
 import { NewUser, UserData } from '../types/user';
 
 export default class UserService {
-  static async fetchUsers(): Promise<AxiosResponse<NewUser[]>> {
+  static fetchUsers(): Promise<AxiosResponse<NewUser[]>> {
     return api.get<NewUser[]>('/users');
   }
 
-  static async getUserData(id: string): Promise<AxiosResponse<UserData>> {
+  static getUserData(id: string): Promise<AxiosResponse<UserData>> {
     return api.get(`${API_URL}${Endpoint.USERS}/${id}`);
   }
 
-  static async updateUserData(
+  static updateUserData(
     id: string,
     userData: SignUpFormInput,
-  ): Promise<AxiosResponse<SignUpFormInput>> {
+  ): Promise<AxiosResponse<UserData>> {
     return api.put(`${API_URL}${Endpoint.USERS}/${id}`, userData);
   }
 }
