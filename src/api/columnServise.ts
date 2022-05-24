503ac000ad630c6d37a5afe9d5d7cb221ed1f44c
Alexander Suslov <sasha_suslov07@mail.ru>
2022-05-24 12:28:30 +0300

    feat: update comlumn service

--- src/api/columnServise.ts
+++ src/api/columnServise.ts
@@ -2,14 +2,15 @@ import { AxiosResponse } from 'axios';
 import api from '.';
 import Endpoint from '../constants/endpoints';
 import { Column, NewColumn } from '../types/columns';
+import { FormData } from '../types/formTypes';
 
 export default class ColumnService {
   static fetchColumns(boardId: string): Promise<AxiosResponse<Column[]>> {
     return api.get(`${Endpoint.BOARDS}/${boardId}${Endpoint.COLUMNS}`);
   }
 
-  static createColumn(boardId: string, data: NewColumn): Promise<AxiosResponse<Column>> {
-    return api.post(`${Endpoint.BOARDS}/${boardId}${Endpoint.COLUMNS}`, { order: +data.order, title: data.title });
+  static createColumn(boardId: string, data: FormData): Promise<AxiosResponse<Column>> {
+    return api.post(`${Endpoint.BOARDS}/${boardId}${Endpoint.COLUMNS}`, data);
   }
 
   static editColumn(
