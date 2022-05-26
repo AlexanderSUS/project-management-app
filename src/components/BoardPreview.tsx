a2b4f54b83cf33fbceefcc8df9b52d2a49f28d5f
PanchenkoAleksandr <aleksandr.panchenko@tut.by>
2022-05-26 10:41:19 +0300

    feat: add create board button ant button translation

--- src/components/BoardPreview.tsx
+++ src/components/BoardPreview.tsx
@@ -15,11 +15,12 @@ interface BoardPreviewProps {
   boardPreview: IBoardPreview;
 }
 
-const Item = styled(Paper)(({ theme }) => ({
+export const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
+  height: '100%',
   color: theme.palette.text.secondary,
 }));
 
--- src/components/BoardPreviewsWrapper.tsx
+++ src/components/BoardPreviewsWrapper.tsx
@@ -1,29 +1,55 @@
 import React from 'react';
-import { Box, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { styled } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
 import BoardPreview from './BoardPreview';
 import { boardPage } from '../constants/text';
 import { IBoardPreview } from '../types/boards';
+import { openModal } from '../store/modalSlice';
+import { NEW_BOARD } from '../constants/formfields';
+import { useAppDispatch } from '../hooks/reduxTypedHooks';
 
 type Props = {
   boardsPreview: IBoardPreview[];
 };
 
-const BoardPreviewsWrapper: React.FC<Props> = ({ boardsPreview }) => (
-  <Box sx={{ width: '100%' }}>
-    {boardsPreview.length ? (
-      <Box sx={{ width: '100%' }}>
-        <Grid container spacing={2}>
-          {boardsPreview.map((preview) => (
-            <Grid item xs={6} sm={4} md={2} key={preview.id}>
-              <BoardPreview boardPreview={preview} />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    ) : (
-      <Box>{boardPage.noBoards}</Box>
-    )}
-  </Box>
-);
+const AddButton = styled(Button)(({ theme }) => ({
+  margin: theme.spacing(1),
+  width: '100%',
+}));
 
+const BoardPreviewsWrapper: React.FC<Props> = ({ boardsPreview }) => {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      {boardsPreview.length ? (
+        <Box sx={{ width: '100%' }}>
+          <Grid container spacing={2}>
+            {boardsPreview.map((preview) => (
+              <Grid item xs={6} sm={4} md={2} key={preview.id}>
+                <BoardPreview boardPreview={preview} />
+              </Grid>
+            ))}
+            <Grid item xs={12} sm={4} md={2}>
+              <AddButton
+                variant="outlined"
+                startIcon={<AddIcon />}
+                onClick={() => {
+                  dispatch(openModal(NEW_BOARD));
+                }}
+              >
+                {t('navText.newBoard')}
+              </AddButton>
+            </Grid>
+          </Grid>
+        </Box>
+      ) : (
+        <Box>{boardPage.noBoards}</Box>
+      )}
+    </Box>
+  );
+};
 export default BoardPreviewsWrapper;
