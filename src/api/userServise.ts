5f95aaad18f64bbf2beb828b640b9fa01b965185
Alexander Suslov <sasha_suslov07@mail.ru>
2022-05-26 15:40:14 +0300

    fix: fix fetchUsers method in userService

--- src/api/userServise.ts
+++ src/api/userServise.ts
@@ -4,11 +4,11 @@ import { API_URL } from '../constants/authorization';
 import Endpoint from '../constants/endpoints';
 import { SignUpFormInput } from '../types/authTypes';
 import { RemoveUserResponse } from '../types/response';
-import { NewUser, UserData } from '../types/user';
+import { UserData } from '../types/user';
 
 export default class UserService {
-  static fetchUsers(): Promise<AxiosResponse<NewUser[]>> {
-    return api.get<NewUser[]>('/users');
+  static fetchUsers(): Promise<AxiosResponse<UserData[]>> {
+    return api.get(Endpoint.USERS);
   }
 
   static getUserData(id: string): Promise<AxiosResponse<UserData>> {
