f0e8d53a4744f056681abba0bc7619b5be97444c
Alexander Suslov <sasha_suslov07@mail.ru>
2022-05-27 13:49:43 +0300

    fix: fix empty board appearance

--- src/components/UserTasks.tsx
+++ src/components/UserTasks.tsx
@@ -5,7 +5,7 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/reduxTypedHooks';
 import { taskSelector } from '../store/taskSlice';
-import { boardSelector, setBoardId } from '../store/boardSlice';
+import { boardSelector, getBoard, setBoardId } from '../store/boardSlice';
 import { columnSelector } from '../store/columnSlice';
 import AppRoutes from '../constants/routes';
 
@@ -31,54 +31,56 @@ const UserTasks: React.FC<Props> = ({ userId }) => {
   const usersTasks = tasks.filter((task) => task.userId === userId);
   const userBoards = boards.filter((board) => tasks.filter((task) => board.id === task.boardId));
 
-  // const dataArray = userBoards.map((bord) => ({
-  //   bord,
-  //   tasks: usersTasks.filter((task) => task.boardId === bord.id),
-  // }));
+  const dataArray = userBoards.map((board) => ({
+    boardId: board.id,
+    boardTitle: board.title,
+    tasks: usersTasks.filter((task) => task.boardId === board.id),
+  }));
 
   const goToBoard = (boardId: string) => {
     dispatch(setBoardId(boardId));
-    navigate(`${AppRoutes.PROJECTS}/${AppRoutes.BOARD_ID}${boardId}`);
+    // TODO FIX BLINKING!
+    dispatch(getBoard()).then(() => {
+      navigate(`${AppRoutes.PROJECTS}/${boardId}`);
+    });
   };
 
   return (
     <Box sx={{ mt: '2rem' }}>
       <Typography variant="h4">Your Tasks: </Typography>
-      {userBoards.map((board) => (
-        <Box key={board.id} sx={{ border: '1px solid blue', m: '1rem' }}>
+      {dataArray.map((data) => (data.tasks.length ? (
+        <Box key={data.boardId} sx={{ border: '1px solid blue', m: '1rem' }}>
           <Typography variant="h5" sx={{ display: 'inline' }}>
             Board:
-            {board.title}
+            {data.boardTitle}
           </Typography>
           <Tooltip title="Go to board">
-            <Button variant="outlined" onClick={() => goToBoard(board.id)}>To board</Button>
+            <Button variant="outlined" onClick={() => goToBoard(data.boardId)}>To board</Button>
           </Tooltip>
           <Box sx={taskWrapperStyles}>
-            {usersTasks.filter((task) => task.boardId === board.id).length
-             && usersTasks.filter((task) => task.boardId === board.id).map(((task) => (
-               <Box key={task.id} sx={{ border: '1px solid red' }}>
-                 <Typography variant="h6">
-                   Title:
-                   {' '}
-                   {task.title}
-                 </Typography>
-                 <Typography>
-                   Description:
-                   {' '}
-                   {task.description}
-                 </Typography>
-                 <Typography>
-                   <b>
-                     List:
-                   </b>
-                   {columns.find((column) => column.id === task.columnId)?.title}
-                 </Typography>
-               </Box>
-             )))}
+            {data.tasks.map(((task) => (
+              <Box key={task.id} sx={{ border: '1px solid red' }}>
+                <Typography variant="h6">
+                  Title:
+                  {' '}
+                  {task.title}
+                </Typography>
+                <Typography>
+                  Description:
+                  {' '}
+                  {task.description}
+                </Typography>
+                <Typography>
+                  <b>
+                    List:
+                  </b>
+                  {columns.find((column) => column.id === task.columnId)?.title}
+                </Typography>
+              </Box>
+            )))}
           </Box>
         </Box>
-
-      ))}
+      ) : null)) }
     </Box>
   );
 };
