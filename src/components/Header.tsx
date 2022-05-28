620d15101207843f5bdfe2be12811b08f85a658d
PanchenkoAleksandr <aleksandr.panchenko@tut.by>
2022-05-28 13:34:32 +0300

    feat: add sticky header with scroll effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
-import { AppBar, Grid } from '@mui/material';
+import {
+  AppBar, Grid, Slide, useScrollTrigger,
+} from '@mui/material';
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -15,6 +17,21 @@ import { NEW_BOARD } from '../constants/formfields';
 import { TOKEN } from '../constants/authorization';
 import { addBoardListener, addModalBoardPageActionListener } from '../store/middlewareListeners';
 import { startBoardListening, startColumsAndTasksListening } from '../store/listenerMiddleware';
+
+type HideOnScrollProps = {
+  children: React.ReactElement;
+};
+
+const HideOnScroll = (props: HideOnScrollProps) => {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+};
 
 const Header: React.FC = () => {
   const { userId } = useAppSelector(authSelector);
@@ -35,7 +52,7 @@ const Header: React.FC = () => {
     } else {
       navigate(AppRoutes.WELCOME);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userId]);
 
   useEffect(() => {
@@ -45,36 +62,40 @@ const Header: React.FC = () => {
 
   useEffect(() => {}, []);
   return (
-    <AppBar position="static" sx={{ padding: '15px' }}>
-      <Grid container spacing={2}>
-        <GridFlexGrow item>
-          <HeaderButton onClick={() => navigate(AppRoutes.WELCOME)} text={t('navText.home')} />
-        </GridFlexGrow>
-        {userId && (
+    <HideOnScroll>
+      <AppBar position="sticky" sx={{ padding: '15px' }}>
+        <Grid container spacing={2}>
+          <GridFlexGrow item>
+            <HeaderButton onClick={() => navigate(AppRoutes.WELCOME)} text={t('navText.home')} />
+          </GridFlexGrow>
+          {userId && (
+            <Grid item>
+              <HeaderButton
+                onClick={() => navigate(AppRoutes.PROJECTS)}
+                text={t('navText.projects')}
+              />
+              <HeaderButton
+                onClick={() => navigate(AppRoutes.EDIT_PROFILE)}
+                text={t('navText.editProfile')}
+              />
+              <HeaderButton
+                text={t('navText.newBoard')}
+                onClick={() => {
+                  dispatch(openModal(NEW_BOARD));
+                }}
+              />
+            </Grid>
+          )}
           <Grid item>
-            <HeaderButton
-              onClick={() => navigate(AppRoutes.PROJECTS)}
-              text={t('navText.projects')}
-            />
-            <HeaderButton
-              onClick={() => navigate(AppRoutes.EDIT_PROFILE)}
-              text={t('navText.editProfile')}
-            />
-            <HeaderButton
-              text={t('navText.newBoard')}
-              onClick={() => { dispatch(openModal(NEW_BOARD)); }}
-            />
+            <LangSwitcher />
           </Grid>
-        )}
-        <Grid item>
-          <LangSwitcher />
+          <Grid item>
+            <AuthButtonsContainer userId={userId} />
+          </Grid>
         </Grid>
-        <Grid item>
-          <AuthButtonsContainer userId={userId} />
-        </Grid>
-      </Grid>
-      <BasicModal />
-    </AppBar>
+        <BasicModal />
+      </AppBar>
+    </HideOnScroll>
   );
 };
 
