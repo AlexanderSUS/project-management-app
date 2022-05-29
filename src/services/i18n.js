a997864de92918b9ab7167e468b6fbc2410c795e
PanchenkoAleksandr <aleksandr.panchenko@tut.by>
2022-05-29 16:29:09 +0300

    feat: add modal translation

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -20,6 +20,14 @@ import {
   modalTextRu,
 } from '../constants/text';
 import { AuthTextEn, AuthTextRu } from '../constants/authorization';
+import {
+  BoardModalTextEn,
+  BoardModalTextRu,
+  ListModalTextEn,
+  ListModalTextRu,
+  TaskModalTextEn,
+  TaskModalTextRu,
+} from '../constants/boards';
 
 i18n
   .use(LanguageDetector)
@@ -35,13 +43,17 @@ i18n
         translation: {
           navText: navTextEn,
           AuthText: AuthTextEn,
+          BoardModalText: BoardModalTextEn,
+          ListModalText: ListModalTextEn,
+          TaskModalText: TaskModalTextEn,
           loginPage: loginPageEn,
           registrationPageText: registrationPageTextEn,
           registrationText: registrationTextEn,
           loginAuthInput: 'login',
           nameErrors: {
-            minLength: 'Value must be no less than 2 symbols',
+            minLength: 'Value must be no less than 1 symbols',
             maxLength: 'Value must be no more than 20 symbols',
+            customLength: 'Value must be no more than 100 symbols',
             pattern: 'name must starts with capital letter',
           },
           loginErrors: {
@@ -62,13 +74,17 @@ i18n
         translation: {
           navText: navTextRu,
           AuthText: AuthTextRu,
+          BoardModalText: BoardModalTextRu,
+          ListModalText: ListModalTextRu,
+          TaskModalText: TaskModalTextRu,
           loginPage: loginPageRu,
           registrationPageText: registrationPageTextRu,
           registrationText: registrationTextRu,
           loginAuthInput: 'Логин',
           nameErrors: {
-            minLength: 'Значение должно быть не меньше 2 символов',
+            minLength: 'Значение должно быть не меньше 1 символа',
             maxLength: 'Значение должно быть не больше 20 символов',
+            customLength: 'Значение должно быть не больше 100 символов',
             pattern: 'Имя должно начинаться с заглавной буквы',
           },
           loginErrors: {
